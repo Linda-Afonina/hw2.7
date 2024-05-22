c9af4c4f1b097ad4cd429ae302bd316c9ad93796
linda <charming.linda85@gmail.com>
2024-05-22 17:59:26 +0300

    Initial commit

---
+++ src/test/java/pro/sky/employeeBookMap/EmployeeBookMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeBookMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
